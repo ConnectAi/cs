38a16d0afff1434bffdfb76f47fed198e27e70e4
Ross Hadden <rosshadden@gmail.com>
2013-06-15 15:39:20 -0400

    Added controller.

--- app.js
+++ app.js
@@ -1,59 +1,60 @@
 ////////////////
 //	NATIVES
 ////////////////
-	var express = require("express"),
-		http = require("http"),
-		path = require("path");
+var express = require("express"),
+	http = require("http"),
+	path = require("path");
 
 
 ////////////////
 //	GLOBALS
 ////////////////
-	global.app = express();
-	global.log = console.log;
+global.app = express();
+global.log = console.log;
 
 
 ////////////////
 //	SETUP
 ////////////////
-	app
-		.set("port", process.env.PORT || 3000)
-		.set("views", __dirname + "/views")
-		.set("view engine", "ejs")
-		.use(express.favicon())
-		// .use(express.logger('dev'))
-		.use(express.bodyParser())
-		.use(express.methodOverride())
-		.use(express.cookieParser("Shh! It's a secret."))
-		.use(express.session())
-		.use(app.router)
-		.use(require("stylus").middleware(__dirname + "/public"))
-		.use(express.static(path.join(__dirname, "public")));
+app.set("port", process.env.PORT || 3000)
+	.set("views", __dirname + "/views")
+	.set("view engine", "ejs")
+	.use(express.favicon())
+	// .use(express.logger('dev'))
+	.use(express.bodyParser())
+	.use(express.methodOverride())
+	.use(express.cookieParser("Shh! It's a secret."))
+	.use(express.session())
+	.use(app.router)
+	.use(require("stylus").middleware(__dirname + "/public"))
+	.use(express.static(path.join(__dirname, "public")));
 
-	//	Run in passed-in environment.
-	//	Defaults to "development".
-	if (process.argv.length === 3) {
-		app.set("env", process.argv[2]);
-	}
+//	Run in passed-in environment.
+//	Defaults to "development".
+if (process.argv.length === 3) {
+	app.set("env", process.argv[2]);
+}
 
 
 ////////////////
 //	MODULES
 ////////////////
-	app.config = require("./config");
+app.config = require("./config");
+app.Controller = require("./modules/Controller");
+app.Model = require("./modules/Model");
 
 
 ////////////////
 //	ROUTES
 ////////////////
-	app.get("/", function(req, res){
-		log("Hello.");
-	});
+app.get("/", function(req, res) {
+	res.send("Hello.");
+});
 
 
 ////////////////
 //	START
 ////////////////
-	http.createServer(app).listen(app.get("port"), function() {
-		log("Framework listening at http://%s:%d [%s]", "localhost", app.get("port"), app.get("env"));
-	});
+http.createServer(app).listen(app.get("port"), function() {
+	log("Framework listening at http://%s:%d [%s]", "localhost", app.get("port"), app.get("env"));
+});
---
+++ system/controllers/user.js
@@ -0,0 +1,9 @@
+var User = {
+
+	test: function(req, res){
+		req.send("User test.");
+	}
+
+};
+
+module.exports = User;
