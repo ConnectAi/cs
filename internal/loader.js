6ef35f2252363ba6ec44ba173716998ce28cf77e
Ross Hadden <rosshadden@gmail.com>
2013-06-19 23:11:25 -0400

    Added my hbs helpers.

--- internal/loader.js
+++ internal/loader.js
@@ -30,7 +30,6 @@ var setupHandlebars = function() {
 	var blocks = {};
 
 	hbs.registerPartial("header", fs.readFileSync("external/views/header.html", "utf8"));
-	hbs.registerPartial("footer", fs.readFileSync("external/views/footer.html", "utf8"));
 
 	hbs.registerHelper("extend", function(name, context) {
 	    var block = blocks[name];
@@ -44,6 +43,92 @@ var setupHandlebars = function() {
 	    var val = (blocks[name] || []).join("\n");
 	    blocks[name] = [];
 	    return val;
+	});
+
+	hbs.registerHelper("log", function(){
+		var args = Array.prototype.slice.call(arguments);
+		return console.log('LOG:', args.slice(0, -1)) || '';
+	});
+
+	//	Handlebars Equality helper.
+	//	{{#iff one}}:  !!one
+	//	{{#iff one two}}:  one === two
+	//	{{#iff one "[operator]" two}}:  one [operator] two
+	hbs.registerHelper('iff', function(){
+		var args = Array.prototype.slice.call(arguments);
+
+		var	left = args[0],
+			operator = '===',
+			right,
+			options = {};
+
+		if(args.length === 2){
+			right = true;
+			options = args[1];
+		}
+
+		if(args.length === 3){
+			right = args[1];
+			options = args[2];
+		}
+
+		if(args.length === 4){
+			operator = args[1];
+			right = args[2];
+			options = args[3];
+		}
+
+		if(options.hash && options.hash['case'] === false){
+			left = (''+left).toLowerCase();
+			right = (''+right).toLowerCase();
+		}
+
+		var operators = {
+			"^==$": function(l, r){ return l == r; },
+			"^!=$": function(l, r){ return l !== r; },
+			"^IS$|^===$": function(l, r){ return l === r; },
+			"^NOT$|^IS NOT$|^!==$|^!$": function(l, r){ return l != r; },
+			"^OR$": function(l, r){ return l || r; },
+			"^AND$|^&&$": function(l, r){ return l && r; },
+			"^MOD$|^%$": function(l, r){ return !(l % r); },
+			"^<$": function(l, r){ return l < r; },
+			"^>$": function(l, r){ return l > r; },
+			"^<=$": function(l, r){ return l <= r; },
+			"^>=$": function(l, r){ return l >= r; },
+			"^typeof$": function(l, r){ return typeof l == r; },
+			"^IN$|^E$": function(l, r){
+				var isPresent = false;
+				if(typeof r === 'object'){
+					if(r.indexOf && r instanceof Array){
+						if(/^\d+$/.test(l)){
+							isPresent = !!~r.indexOf(+l) || !!~r.indexOf(''+l);
+						}
+						return isPresent || !!~r.indexOf(l);
+					}else{
+						return l in r;
+					}
+				}
+			}
+		};
+
+		var op, result, expression;
+		for(op in operators){
+			expression = RegExp(op, 'i');
+
+			if(expression.test(operator)){
+				result = operators[op](left, right);
+
+				if(result){
+					return options.fn(this);
+				}else{
+					return options.inverse(this);
+				}
+			}
+		}
+
+		if(!operators[operator]){
+			throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+		}
 	});
 };
 
