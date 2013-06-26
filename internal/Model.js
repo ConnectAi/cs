9e4e8083c6022fc0ebcb7eac63a66d76d3203b0f
Ross Hadden <rosshadden@gmail.com>
2013-06-25 22:27:19 -0400

    Made DB funcitons return promises.

--- internal/Model.js
+++ internal/Model.js
@@ -9,20 +9,25 @@ class Model {
 	}
 
 	query(q, fn = ()=>{}) {
+		var def = Q.defer();
+
 		db.query(q, (err, res) => {
 			this.log(q); if(err) this.log(err);
 			if(err) {
 				this.log(err);
 				this.error(err, q);
+				def.reject(err);
 			} else {
 				fn(res);
+				def.resolve(res);
 			}
 		});
+
+		return def.promise;
 	}
 
 	save(data, table = this.table, primaryKey = this.primaryKey) {
 		var q = "";
-		var def = Q.defer();
 		var id = 0;
 
 		// if we have an id, update
@@ -54,48 +59,71 @@ class Model {
 		// run the query
 		db.query(q, data, (err, result) => {
 			// if error
-			if(err) this.log(err);
-
-			// resolve the defered with our stuff
-			def.resolve(result.insertId || id);
+			if (err) {
+				this.log(err);
+				def.reject(err);
+			} else {
+				// otherwise resolve the defered with our stuff
+				def.resolve(result.insertId || id);
+			}
 		});
 
 		return def.promise;
 	}
 
 	find(where, fn, table = this.table) {
+		var def = Q.defer();
+
 		var q = "SELECT * FROM " + table + " WHERE 1 = 1 && " + where;
 		this.querySingle(q, function(row) {
 			fn(row);
+			def.resolve(row);
 		});
+
+		return def.promise;
 	}
 
 	findById(id, fn, table = this.table) {
-		this.find("id = " + id, fn, table);
+		return this.find("id = " + id, fn, table);
 	}
 
 	// db query to get a single value
 	queryValue(q, fn) {
+		var def = Q.defer();
+
 		this.querySingle(q, function(row) {
 			for(var i in row) {
 				fn(row[i]);
+				def.resolve(row[i]);
 				break;
 			}
 		});
+
+		return def.promise;
 	}
 
 	// db query to get a single array
 	querySingle(q, fn) {
+		var def = Q.defer();
+
 		this.query(q, function(rows) {
 			fn(rows[0]);
+			def.resolve(rows[0]);
 		});
+
+		return def.promise;
 	}
 
 	// db query to get many arrays of arrays of arrays of...
 	queryMulti(q, fn) {
+		var def = Q.defer();
+
 		this.query(q, function(res) {
 			fn(res);
+			def.resolve(res);
 		});
+
+		return def.promise;
 	}
 
 	log() {
