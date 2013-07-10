d35ca75986e8571f3c766c41d3784e4997a54f0f
Ross Hadden <rosshadden@gmail.com>
2013-07-10 00:44:37 -0400

    Started adding a Time class, with getters for common properties, including padded ones.

--- internal/util/index.js
+++ internal/util/index.js
@@ -22,9 +22,72 @@ var log = function(line) {
 	fs.appendFile("app.log", `${date}:\t${line}\n`);
 };
 
+private @date;
+class Time {
+	constructor(date = new Date()) {
+		this.@date = date;
+	}
+
+	pad(input) {
+		if (input < 10) {
+			return "0" + input;
+		}
+		return "" + input;
+	}
+
+	get year() {
+		return this.@date.getFullYear();
+	}
+
+	get month() {
+		return this.@date.getMonth();
+	}
+
+	get day() {
+		return this.@date.getDate();
+	}
+
+	get hour() {
+		return this.@date.getHours();
+	}
+
+	get minute() {
+		return this.@date.getMinutes();
+	}
+
+	get second() {
+		return this.@date.getSeconds();
+	}
+
+	get years() {
+		return this.pad(this.year);
+	}
+
+	get months() {
+		return this.pad(this.month);
+	}
+
+	get days() {
+		return this.pad(this.day);
+	}
+
+	get hours() {
+		return this.pad(this.hour);
+	}
+
+	get minutes() {
+		return this.pad(this.minute);
+	}
+
+	get seconds() {
+		return this.pad(this.second);
+	}
+};
+
 module.exports = {
 	capitalize,
 	validateEmail,
 	randomString,
-	log
+	log,
+	Time
 };
