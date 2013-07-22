f0db23050fac5972750da384a57e0ed44e46497d
Ross Hadden <rosshadden@gmail.com>
2013-07-21 22:33:28 -0400

    Made loader take an options hash, and allowed the finall reduce to be optional.

--- internal/util/loader.js
+++ internal/util/loader.js
@@ -1,36 +1,45 @@
 var fs = require("fs");
 var path = require("path");
 
-var dirSync = function(dir, whitelist = /^([\w][\w\-_. ]*)\.js$/) {
+var dirSync = function(dir, options = {}) {
 	var filepath = path.resolve("external/" + dir);
+
+	if (!("whitelist" in options)) options.whitelist = /^([\w][\w\-_. ]*)\.js$/;
+	if (!("reduce" in options)) options.reduce = true;
 
 	var filter;
 	// Filter can either be a function, regex, or an array of filenames.
-	if (typeof whitelist === "function") {
-		filter = whitelist;
-	} else if (whitelist instanceof RegExp) {
+	if (typeof options.whitelist === "function") {
+		filter = options.whitelist;
+	} else if (options.whitelist instanceof RegExp) {
 		filter = (file) => {
-			return whitelist.test(file);
+			return options.whitelist.test(file);
 		};
-	} else if (Array.isArray(whitelist)) {
+	} else if (Array.isArray(options.whitelist)) {
 		filter = (file) => {
-			return !!~whitelist.indexOf(file);
+			return !!~options.whitelist.indexOf(file);
 		};
 	} else {
 		filter = () => true;
 	}
 
 	// Build list of files in this directory.
-	return fs
+	var files = fs
 		.readdirSync(filepath)
 		.filter(filter)
 		.map(function(file){
-			return file.replace(/\.\w+/, "");
-		})
-		.reduce((files, file) => {
-			files[file] = require(filepath + "/" + file)
-			return files;
-		}, {});
+			return file.replace(/\.\w+$/, "");
+		});
+
+	if (options.reduce) {
+		files = files
+			.reduce((files, file) => {
+				files[file] = require(filepath + "/" + file)
+				return files;
+			}, {});
+	}
+
+	return files;
 };
 
 module.exports = {
