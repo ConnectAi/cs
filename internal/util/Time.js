9f724bd12e4b5e13e1b786787fbf033bc3ac59c3
Ross Hadden <rosshadden@gmail.com>
2013-08-04 12:41:49 -0400

    Reorganized Time class, added some ISO standards methods, and added sectional comments.

--- internal/util/Time.js
+++ internal/util/Time.js
@@ -6,76 +6,85 @@ class Time {
 		this.@date = date;
 	}
 
-	pad(input) {
-		if (input < 10) {
-			return "0" + input;
-		}
-		return ""+input;
-	}
-
-	get year() {
-		return this.@date.getFullYear();
-	}
-
-	get month() {
-		return this.@date.getMonth();
-	}
-
-	get day() {
-		return this.@date.getDate();
-	}
-
-	get hour() {
-		return this.@date.getHours();
-	}
-
-	get minute() {
-		return this.@date.getMinutes();
-	}
-
-	get second() {
-		return this.@date.getSeconds();
-	}
-
-	get years() {
-		return ""+this.year;
-	}
-
-	get months() {
-		return this.pad(this.month);
-	}
-
-	get days() {
-		return this.pad(this.day);
-	}
-
-	get hours() {
-		return this.pad(this.hour);
-	}
-
-	get minutes() {
-		return this.pad(this.minute);
-	}
-
-	get seconds() {
-		return this.pad(this.second);
-	}
-
-	get time() {
-		return `${this.hours}:${this.hours}:${this.seconds}`;
-	}
-
-	get date() {
-		return `${this.years}-${this.months}-${this.days}`;
-	}
-
-	format(...args) {
-		return dateformat(this.@date, ...args);
-	}
-
 	toString() {
-		return `${this.date} ${this.time}`;
+		return `${this.isoDate} ${this.isoTime}`;
 	}
+
+	// Utility.
+		pad(input) {
+			if (input < 10) {
+				return "0" + input;
+			}
+			return ""+input;
+		}
+
+	// Convenience.
+		format(...args) {
+			return dateformat(this.@date, ...args);
+		}
+
+	// Singular (numeric output).
+		get year() {
+			return this.@date.getFullYear();
+		}
+
+		get month() {
+			return this.@date.getMonth();
+		}
+
+		get day() {
+			return this.@date.getDate();
+		}
+
+		get hour() {
+			return this.@date.getHours();
+		}
+
+		get minute() {
+			return this.@date.getMinutes();
+		}
+
+		get second() {
+			return this.@date.getSeconds();
+		}
+
+	// Plural (string output).
+		get years() {
+			return ""+this.year;
+		}
+
+		get months() {
+			return this.pad(this.month);
+		}
+
+		get days() {
+			return this.pad(this.day);
+		}
+
+		get hours() {
+			return this.pad(this.hour);
+		}
+
+		get minutes() {
+			return this.pad(this.minute);
+		}
+
+		get seconds() {
+			return this.pad(this.second);
+		}
+
+	// Standards.
+		get iso() {
+			return this.@date.toISOString().reaplace(/Z.*$/, "");
+		}
+
+		get isoDate() {
+			return `${this.years}-${this.months}-${this.days}`;
+		}
+
+		get isoTime() {
+			return `${this.hours}:${this.hours}:${this.seconds}`;
+		}
 };
 
 Time.format = dateformat;
