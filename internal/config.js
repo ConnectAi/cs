4a8487aef37603f60de4d1049a597f16e730a70d
Ross Hadden <rosshadden@gmail.com>
2013-10-03 08:28:15 -0400

    Added path (and default path) to config.

--- internal/config.js
+++ internal/config.js
@@ -4,13 +4,15 @@ external.routes = require(`${app.dirs.external}/controllers`);
 var defaults = {
 	name: "[name]",
 	port: process.env.PORT || 3000,
-	env: process.env.NODE_ENV || "development"
+	env: process.env.NODE_ENV || "development",
+	path: "/"
 };
 
 var config = {
 	name: external.name || defaults.name,
 	port: external.port || defaults.port,
 	env: (process.argv.length === 3) ? process.argv[2] : defaults.env,
+	path: external.path || defaults.path,
 	db: external.db,
 	routes: external.routes
 };
@@ -29,5 +31,6 @@ config.db = config.db[config.db.adapter];
 server.set("name", config.name);
 server.set("port", config.port);
 server.set("env", config.env);
+server.set("path", config.path);
 
 module.exports = config;
