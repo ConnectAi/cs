fd28a37022b4e0b18b4039d0fe2b2a8b124042d1
Ross Hadden <rosshadden@gmail.com>
2013-10-29 12:02:53 -0400

    Added `cs init`.

--- bin/cs.js
+++ bin/cs.js
@@ -1,8 +1,33 @@
 #!/usr/bin/env node
 
-var program = require("commander");
+var fs = require("fs")
+ ,  path = require("path")
+ ,  npm = require("npm")
+ ,  program = require("commander")
+ ,  ghdown = require("github-download")
+;
 
 program
 	.version(require("../package").version)
+;
+
+program.command("init")
+	.description("Create an empty cornerstone project or reinitialize an existing one.")
+	.action(function() {
+		var dir = process.cwd();
+
+		ghdown("git@github.com:ConnectAi/cornerstone-skeleton.git", process.cwd())
+			.on("error", function(err) {
+				console.error(err);
+			})
+			.on("end", function() {
+				fs.unlink(".gitignore");
+				npm.load(require(path.join(dir, "./package")), function() {
+					npm.commands.install();
+				});
+			})
+		;
+	})
+;
 
 program.parse(process.argv)
