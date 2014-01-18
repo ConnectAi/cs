af0d2d8c1073681727f9e73bfc278775eaa6df6e
Ross Hadden <rosshadden@gmail.com>
2014-01-18 13:52:09 -0500

    Abstracted the defaults overriding process.

--- internal/config.js
+++ internal/config.js
@@ -1,5 +1,9 @@
 var external = require(`${app.dirs.external}/config`);
 
+// This finagling is to avoid changing apps that use cs.
+var environments = external.env || {};
+external.env = (process.argv.length === 3) ? process.argv[2] : undefined;
+
 var defaults = {
 	name: "[name]",
 	port: process.env.PORT || 3000,
@@ -9,21 +13,24 @@ var defaults = {
 	useSockets: true
 };
 
-var config = {
-	name: external.name || defaults.name,
-	port: external.port || defaults.port,
-	env: (process.argv.length === 3) ? process.argv[2] : defaults.env,
-	path: external.path || defaults.path,
-	db: external.db,
-	useRedis: external.useRedis || defaults.useRedis,
-	useSockets: external.useSockets || defaults.useSockets
+var override = function(setting) {
+	return (typeof external[setting] !== "undefined") ? external[setting] : defaults[setting];
 };
 
+var config = {
+	name: override("name"),
+	port: override("port"),
+	env:  override("env"),
+	path: override("path"),
+	db: external.db,
+	useRedis: override("useRedis"),
+	useSockets: override("useSockets")
+};
 
-for (let env in external.env) {
+for (let env in environments) {
 	if (env === config.env) {
-		for (let override in external.env[env]) {
-			config[override] = external.env[env][override];
+		for (let override in environments[env]) {
+			config[override] = environments[env][override];
 		}
 	}
 }
