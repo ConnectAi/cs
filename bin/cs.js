29de2df8ec1a5766d4aeeee8f67c3dad9bd72e45
Ross Hadden <rosshadden@gmail.com>
2014-02-01 18:26:39 -0500

    Added `cs run`.

--- bin/cs.js
+++ bin/cs.js
@@ -4,7 +4,8 @@ var fs = require("fs"),
 	path = require("path"),
 	npm = require("npm"),
 	program = require("commander"),
-	request = require("request")
+	request = require("request"),
+	nodemon = require("nodemon")
 ;
 
 var urls = {
@@ -21,7 +22,7 @@ program
 ;
 
 program.command("init [name] [path]")
-	.description("Create an empty cornerstone project or reinitialize an existing one.")
+	.description("Create an empty Cornerstone project or reinitialize an existing one.")
 	.option("-b, --bare", "bare project")
 	.action(function(name, directory, options) {
 		// If a directory was given but no name, swap variables.
@@ -49,58 +50,75 @@ program.command("init [name] [path]")
 		// Make the directory if it does not exist.
 		if (!fs.existsSync(directory)) fs.mkdirSync(directory);
 
-		if (options.bare) {
-			console.log("bare project");
-		} else {
-			request.get({
-				url: urls.tree,
-				json: true,
-				headers: {
-					"User-Agent": "request"
-				}
-			}, function(err, res, body) {
-				var dirs, files;
-				if (!err && res.statusCode == 200) {
-					// Split files from git into folders (tree) and files (blob).
-					var nodes = body.tree.reduce(function(nodes, branch) {
-						if (!(branch.type in nodes)) nodes[branch.type] = [];
-						nodes[branch.type].push(branch.path);
-						return nodes;
-					}, {});
+		request.get({
+			url: urls.tree,
+			json: true,
+			headers: {
+				"User-Agent": "request"
+			}
+		}, function(err, res, body) {
+			var dirs, files;
+			if (!err && res.statusCode == 200) {
+				// Split files from git into folders (tree) and files (blob).
+				var nodes = body.tree.reduce(function(nodes, branch) {
+					if (!(branch.type in nodes)) nodes[branch.type] = [];
+					nodes[branch.type].push(branch.path);
+					return nodes;
+				}, {});
 
-					// First make each directory.
-					nodes.tree.forEach(function(dir) {
-						var where = directory + "/" + dir;
-						if (!fs.existsSync(where)) {
-							fs.mkdirSync(where);
-							console.log("Writing", where);
-						} else {
-							console.log("Skipping", where);
+				// First make each directory.
+				nodes.tree.forEach(function(dir) {
+					var where = directory + "/" + dir;
+					if (!fs.existsSync(where)) {
+						fs.mkdirSync(where);
+						console.log("Writing", where);
+					} else {
+						console.log("Skipping", where);
+					}
+				});
+
+				// Then save each file.
+				nodes.blob.forEach(function(file) {
+					var where = directory + "/" + file;
+					if (!fs.existsSync(where)) {
+						var writer = fs.createWriteStream(where);
+						request(urls.raw + file).pipe(writer);
+
+						if (name && file === "config.json") {
+							writer.on("finish", function() {
+								var config = require(where);
+								config.name = name;
+								fs.writeFile(where, JSON.stringify(config, null, "\t"));
+							});
 						}
-					});
+						console.log("Writing", where);
+					} else {
+						console.log("Skipping", where);
+					}
+				});
+			}
+		});
+	})
+;
 
-					// Then save each file.
-					nodes.blob.forEach(function(file) {
-						var where = directory + "/" + file;
-						if (!fs.existsSync(where)) {
-							var writer = fs.createWriteStream(where);
-							request(urls.raw + file).pipe(writer);
+program.command("run [app]")
+	.description("Run a Cornerstone app.")
+	.action(function(app, options) {
+		nodemon({
+			script: "index.js",
+			ext: "js json"
+		});
 
-							if (name && file === "config.json") {
-								writer.on("finish", function() {
-									var config = require(where);
-									config.name = name;
-									fs.writeFile(where, JSON.stringify(config, null, "\t"));
-								});
-							}
-							console.log("Writing", where);
-						} else {
-							console.log("Skipping", where);
-						}
-					});
-				}
-			});
-		}
+		nodemon
+		.on("start", function() {
+			console.log("App has started.");
+		})
+		.on("quit", function() {
+			console.log("App has quit.");
+		})
+		.on("restart", function(files) {
+			console.log("App has restarted due to", files);
+		});
 	})
 ;
 
